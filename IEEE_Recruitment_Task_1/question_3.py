fa3ec4d3819c8a46a43e73b94f0ba0187119fe9d
Shiven1023 <shiveng1023@gmail.com>
2025-09-01 14:49:21 +0530

    Update question_3.py

--- IEEE_Recruitment_Task_1/question_3.py
+++ IEEE_Recruitment_Task_1/question_3.py
@@ -1,7 +1,7 @@
 import numpy as np
 my_arr = np.random.randint(1,101,size=(5,5))
 #Original Matrix
-print("Original Matrix: ",my_arr)
+print("Original Matrix: \n",my_arr)
 # max,min and mean values
 print("min value: ",my_arr.min())
 print("max value: ",my_arr.max())
@@ -9,10 +9,10 @@ print("mean value: ",my_arr.mean())
 
 # Normalize between 0 and 1  --> (x - min) / (max - min)
 normalized_arr= (my_arr - my_arr.min()) / (my_arr.max() - my_arr.min())
-print("Normalized Matrix: ",normalized_arr)
+print("Normalized Matrix: \n",normalized_arr)
 
 # Flatten to 1D array and sort
 flattened_arr = normalized_arr.reshape(-1)
 sorted_arr = np.sort(flattened_arr)
-print("Flattened Matrix: ",flattened_arr)
-print("sorted Matrix: ",sorted_arr)
+print("Flattened Matrix: \n",flattened_arr)
+print("sorted Matrix: \n",sorted_arr)
