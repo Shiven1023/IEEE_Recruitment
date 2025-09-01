0a26ddf6b3a0b8f05fbaa3cee0990510c1211f9d
Shiven1023 <shiveng1023@gmail.com>
2025-09-01 18:24:14 +0530

    Update question_5.py

--- IEEE_Recruitment_Task_1/question_5.py
+++ IEEE_Recruitment_Task_1/question_5.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 arr = np.random.normal(loc = 0,scale = 1,size = 1000)
-plt.scatter(range(1000),arr,alpha = 0.6)
+plt.scatter(range(1000),arr,alpha = 0.5)
 plt.title("ScatterPlot of 1000 Random Numbers")
 plt.xlabel("X axis")
 plt.ylabel("Y axis")
